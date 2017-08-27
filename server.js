0dcf8c3ba19d345dd4416ab55f7765c677ddd1ff
srujanaaa <30764475+srujanaaa@users.noreply.github.com>
2017-08-27 17:28:26 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -31,6 +31,32 @@ app.get('/test-db',function(req,res){
        }
     });
 });
+
+app.get('articles/:articleName',function(req,res){
+    //articleName-article-one
+    //articles[articleName]=={}content object for object one
+    var articleName=req.params.articleName;
+    
+    
+    pool.query("SELECT* FROM article WHERE title= "+req.params.articleName, function (err,result){
+        if(err) {
+            res.status(500).send(err.toString());
+        } else {
+            if(result.rows.length==0) {
+                res.status(404).send('article not found');
+            } else {
+                var articleData=result.rows[0];
+                res.send(createTemplate(articleData));
+            }
+        }
+        
+    });
+   
+});
+
+
+
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
